a5295c03bbabd56e93feaa2f816e0db6a493a755
Jonathon Vandezande <jevandezande@gmail.com>
2014-11-03 11:34:58 -0500

    Fully working Gamessifier.

--- gamessifier.py
+++ gamessifier.py
@@ -3,48 +3,65 @@
 import os
 from molecule import Molecule
 from helper import atomic_number, convert_name
+from basis import BasisSet
 
 
 class Gamessifier():
     """Class for making Gamess input files"""
     def __init__(self):
-        self.mol = None
-        self.basis = None
-        self.other = None
-        pass
+        self.mol = Molecule()
+        self.basis_set = BasisSet()
+        self.ecp = {}
+        self.other = ''
 
     def read_mol(self, geom_file='geom.xyz'):
         """Reads a geometry file and generates a molecule"""
-        self.mol = Molecule()
-        self.mol.read_xyz(geom_file)
+        self.mol.read(geom_file)
 
-    def read_basis(self, basis_file='basis.gbs'):
+    def read_basis_set(self, basis_file='basis.gbs'):
         """Reads a basis file and makes a dictionary with the form atom:basis_functions"""
-        if os.path.isfile(basis_file):
-            raw_basis = open('basis.gbs', 'r').read()
-        else:
-            raise IOError('Cannot read basis')
+        self.basis_set.read_basis_set(basis_file, style='gamess')
 
-        self.basis = {}
-        for atom in raw_basis.split('\n\n'):
-            atom_name = atom.split('\n')[0].strip()
-            self.basis[atom_name] = '    ' + '\n    '.join(atom.split('\n')[1:])
+    def read_ecp(self, ecp_file='ecp.dat'):
+        """Reads an ecp file and makes a dictionary with the form atom:ecp"""
+        lines = open(ecp_file).readlines()
+        self.ecp = {}
+        i = 0
+        while i < len(lines):
+            name, ecp_type = lines[i].split()[:2]
+            name = name.split('-')[0]
+            if ecp_type == 'GEN':
+                self.ecp[name] = ''
+                done = False
+                while not done:
+                    i += 1
+                    if lines[i][0] == ' ':
+                        self.ecp[name] += lines[i]
+                    else:
+                        done = True
+            elif ecp_type == 'NONE':
+                self.ecp[name] = lines[i]
+                i += 1
+            else:
+                raise SyntaxError('Invalid ecp type, only GEN and NONE are currently accepted.')
 
-    def read_other(self, other_file):
+    def read_other(self, other_file=''):
         """Reads a file that will be prepended to the input file"""
-        with open(other_file, 'r') as f:
-            self.other = f.read()
+        if other_file:
+            self.other = open(other_file).read()
 
     def write_input(self, input_file='input.inp', comment=''):
         """Makes an input file with the given geometry and basis"""
-        data = ''
-        for i in range(len(self.mol)):
-            name, x, y, z = self.mol[i]
+        data = ' $DATA\n{}\nC1\n'.format(comment)
+        ecp = ' $ECP\n'
+        for name, x, y, z in self.mol:
             an = atomic_number(name)
-            atom_basis = self.basis[convert_name(an).upper()].strip()
-            data += '{} {}     {}  {}  {}\n{}\n\n'.format(name, an, x, y, z, atom_basis)
-
-        input_data = '{}\n$DATA\n{}\nC1\n{}\n $END\n'.format(self.other, comment, data)
+            atom_basis = self.basis_set[name].print(print_name=False)
+            data += '{} {}     {}  {}  {}\n{}\n'.format(name, an, x, y, z, atom_basis)
+            ecp += self.ecp[name].strip() + '\n'
+        data += ' $END\n'
+        ecp += ' $END\n'
+        input_data = '{}\n{}\n{}'.format(self.other, data, ecp)
         open(input_file, 'w').write(input_data)
 
 
--- tests/gamessifier_unittest.py
+++ tests/gamessifier_unittest.py
@@ -3,26 +3,88 @@ from sys import path
 path.append('../..')
 from gamessifier import Gamessifier
 from molecule import Molecule
+from basis import Contraction, Basis, BasisSet
 import os
 
 
-class TestMolecule(unittest.TestCase):
+class TestGamessifier(unittest.TestCase):
     """Tests the Molecule class"""
 
     def setUp(self):
         """Set up for every test"""
         self.g = Gamessifier()
-        self.water_xyz = Molecule([['H', 0, 0, 0], ['O', 0, 0, 1], ['H', 0, 1, 1]])
+        self.formaldehyde_xyz = Molecule([['O', 0, 1.394, 0], ['C', 0, 0, 0], ['H', 0.994, -0.492, 0], ['H', -0.994, -0.492, 0]])
 
     def test_read_mol(self):
         """Test reading a molecule from a file"""
         tmp_geom_file = 'geom.xyz.tmp'
-        self.water_xyz.write(tmp_geom_file)
+        self.formaldehyde_xyz.write(tmp_geom_file)
         self.g.read_mol(tmp_geom_file)
-        self.assertEqual(self.g.mol, self.water_xyz)
+        self.assertEqual(str(self.g.mol), str(self.formaldehyde_xyz))
 
         os.remove(tmp_geom_file)
 
-    def test_read_basis(self, basis_file):
+    def test_read_basis_set(self):
+        """Test reading a basis from a file"""
         tmp_basis_file = 'basis.gbs.tmp'
+        basis_set = BasisSet()
+        basis_set['B'] = Basis('B', [Contraction('S', [0.2, 0.4], [0.3, 0.7])])
+        open(tmp_basis_file, 'w').write(basis_set.print('gamess'))
+        self.g.read_basis_set(tmp_basis_file)
+        self.assertEqual(basis_set, self.g.basis_set)
+
+        os.remove(tmp_basis_file)
+
+    def test_read_ecp(self):
+        """Testing reading an ecp file"""
+        tmp_ecp_file = 'ecp.dat.tmp'
+        ecp = """\
+H-ECP NONE
+C-ECP GEN   10  2
+    3      ----- d-ul potential     -----
+      -10.0000000        1    357.3914469
+      -60.2990287        2     64.6477389
+      -10.4217834        2     16.0960833
+O-ECP NONE"""
+        open(tmp_ecp_file, 'w').write(ecp)
+        self.g.read_ecp(tmp_ecp_file)
+        os.remove(tmp_ecp_file)
+
+    def test_write_input(self):
+        """Test writing an input to a basis file"""
+        tmp_basis_file = 'basis.gbs.tmp'
+        basis_set = BasisSet()
+        basis_set['H'] = Basis('H', [Contraction('S', [1], [1])])
+        basis_set['O'] = Basis('O', [Contraction('S', [1], [1]), Contraction('S', [2], [1])])
+        basis_set['C'] = Basis('C', [Contraction('S', [1], [1]), Contraction('SP', [1, 2], [0.4, 0.6], [0.1, 0.9])])
+        open(tmp_basis_file, 'w').write(basis_set.print('gamess'))
+        tmp_geom_file = 'geom.xyz.tmp'
+        self.formaldehyde_xyz.write(tmp_geom_file)
+        tmp_ecp_file = 'ecp.dat.tmp'
+        ecp = """\
+H-ECP NONE
+C-ECP GEN   10  2
+    3      ----- d-ul potential     -----
+      -10.0000000        1    357.3914469
+      -60.2990287        2     64.6477389
+      -10.4217834        2     16.0960833
+O-ECP NONE"""
+        open(tmp_ecp_file, 'w').write(ecp)
+        tmp_other_file = 'other.dat'
+        open(tmp_other_file, 'w').write('Hello\n')
+
+        self.g.read_mol(tmp_geom_file)
+        self.g.read_basis_set(tmp_basis_file)
+        self.g.read_ecp(tmp_ecp_file)
+        self.g.read_other(tmp_other_file)
+
+        tmp_input_file = 'input.inp.tmp'
+        self.g.write_input(tmp_input_file)
+
+        os.remove(tmp_geom_file)
+        os.remove(tmp_basis_file)
+        os.remove(tmp_ecp_file)
+        os.remove(tmp_other_file)
+        os.remove(tmp_input_file)
+
 
