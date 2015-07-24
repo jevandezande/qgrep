06cff828abe02201eeda343987b4a4beb2d3f3f4
Kevin Moore <kevmoore@uga.edu>
2015-07-24 15:54:47 -0400

    Adds get_ir to cfour

--- qgrep/cfour.py
+++ qgrep/cfour.py
@@ -38,3 +38,37 @@ def get_geom(lines, geom_type='xyz', units='bohr'):
     return geom
 
 
+def get_ir(lines):
+  """ Returns all the frequencies and geometries in xyz format. """
+
+  geom = get_geom(lines)
+  num_atoms = len(geom)
+
+  vibrations_start = 0
+  vib_modes_start = 0
+  vib_modes_end = 0
+  vib_freqs_start = 0
+  vib_freqs_end = 0
+  hybrid_hessian_vib_modes_end = 0
+  for i in reversed(list(range(len(lines)))):
+      line = lines[i]
+      if 'Gradient vector in normal coordinate representation' in line:
+          vib_modes_end = i - 1
+      elif 'Normal Coordinates' in line:
+          vib_modes_start = i + 2
+      elif 'Zero-point vibrational energy' in line:
+          vib_freqs_end = i - 1
+      elif 'Cartesian force constants:' in line:
+          vib_freqs_start = i + 1 + 6 # The + 6 is to exclude the 0.000 cm^-1 freqs 
+          break
+
+
+  freqs = []
+  for i in range(vib_freqs_start, vib_freqs_end + 1):
+    freqs.append(lines[i].split()[1])
+
+  freq_num = len(freqs)
+  freqs.reverse()
+
+  return freqs
+
