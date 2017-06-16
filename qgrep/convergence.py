1b9f104adea217b2f92a7236a42d66e90e5f36f2
Jonathon Vandezande <jevandezande@gmail.com>
2017-06-16 17:33:22 +0200

    Updates check_convergence to use cclib.

--- qgrep/convergence.py
+++ qgrep/convergence.py
@@ -16,10 +16,9 @@ class Step:
 
     def __str__(self):
         #form = "{energy:} {delta_e:} {rms_grad:} {max_grad:} {rms_step:} {max_step:}"
-        form = "{:> 10.8f} {:> 10.8f} {:> 10.8f} {:> 10.8f} {:> 10.8f}"
+        form = "{:> 13.10f} {:> 13.10f} {:> 13.10f} {:> 13.10f} {:> 13.10f}"
         return form.format(self.delta_e, self.rms_grad, self.max_grad,
                            self.rms_step, self.max_step)
-
 
 
 class Convergence:
@@ -31,32 +30,40 @@ class Convergence:
         self.criteria = criteria
 
     def __str__(self):
-        header = "  Δ energy   RMS energy   RMS grad    MAX grad    RMS step    MAX Step\n"
-        return header + '\n'.join(str(step) for step in self.steps)
+        header = "         Δ energy     RMS grad      MAX grad      RMS step      MAX Step\n"
+        return header + '\n'.join('{:>3}: {}'.format(i, step) for i, step in enumerate(self.steps))
 
     def plot(self):
-        try:
-            from matplotlib import pyplot as plt
+        from matplotlib import pyplot as plt
 
-            f, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, sharex='col', sharey='row')
-            x = range(len(self.steps))
-            #f.set_title('Convergence')
-            ax1.set_title(r'$\Delta$ Energy')
-            ax1.plot(x, self.delta_e)
-            ax2.set_title('RMS Grad')
-            ax2.plot(x, self.rms_grad)
-            ax3.set_title('RMS Step')
-            ax3.plot(x, self.rms_step)
+        f, (ax0, ax1) = plt.subplots(1, 2, sharex='col')
+        f.suptitle('Convergence', fontsize=16)
+        plt.xlabel('Step')
+        x = range(len(self.steps))
 
-            #ax4.set_title('RMS Energy')
-            #ax4.plot(x, self.rms_energy)
-            ax5.set_title('Max Grad')
-            ax5.plot(x, self.max_grad)
-            ax6.set_title('Max Step')
-            ax6.plot(x, self.max_step)
-            plt.show()
-        except:
-            raise ImportError("No module named 'matplotlib'")
+        ax0.set_yscale('symlog', linthreshy=1e-5)
+        ax0.set_title(r'$\Delta$ Energy')
+        ax0.plot(x, self.delta_e)
+
+        ax1.set_ylim(0, 1)
+        ax1.set_yscale('symlog', linthreshy=1e-6)
+        ax1.set_title('Gradient')
+        ax1.plot(x, self.rms_grad, 'b-')
+        ax1.plot(x, self.max_grad, 'b--')
+        ax1.plot(x, self.rms_step, 'r-')
+        ax1.plot(x, self.max_step, 'r--')
+        # TODO: generalize for more than ORCA
+        ax1.plot(x, [self.criteria[1]]*len(self.steps), 'b-.')
+        ax1.plot(x, [self.criteria[2]]*len(self.steps), 'b:')
+        ax1.plot(x, [self.criteria[3]]*len(self.steps), 'r-.')
+        ax1.plot(x, [self.criteria[4]]*len(self.steps), 'r:')
+
+        #ax3.axis('off')
+        #ax4.set_title('Max Grad')
+        #ax4.plot(x, self.max_grad)
+        #ax5.set_title('Max Step')
+        #ax5.plot(x, self.max_step)
+        plt.show()
 
     @property
     def delta_e(self):
