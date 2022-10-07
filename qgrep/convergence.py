9c5be7818d8e26b50fe6e1fae8e9900233f9c881
Jonathon Vandezande <jevandezande@gmail.com>
2022-10-07 11:17:21 -0400

    Updates convergence code.

--- qgrep/convergence.py
+++ qgrep/convergence.py
@@ -1,103 +1,112 @@
+from dataclasses import dataclass
+
 import numpy as np
 
-from collections import OrderedDict
 
-
+@dataclass
 class Step:
     """
     An object that stores a geometry convergence step
     :param params: convergence parameters
     :param criteria: criteria for convergence
     """
-    def __init__(self, params, criteria):
-        self.__dict__.update(params)
-        self.params = params
-        self.criteria = criteria
+
+    params: dict[str, float]
+    criteria: list[float]
+
+    def __post_init__(self):
+        self.__dict__ |= self.params
+        assert len(self.params) == len(self.criteria)
+        print(list(zip(self.params.items(), self.criteria)))
+
+    def __iter__(self):
+        yield from zip(self.params.items(), self.criteria)
 
     def __str__(self):
-        out = ''
-        for (key, value), criterion in zip(self.params.items(), self.criteria):
-            # integers
-            if key in ['scf_steps']:
+        out = ""
+        for (key, value), criterion in self:
+            if key in ["scf_steps"]:
                 pass
-            out += f'{value:> 9.2e}'
-            out += '*' if abs(value) < criterion else ' '
+            out += f"{value:> 9.2e}"
+            out += "*" if abs(value) < criterion else " "
 
-        out += f'|{value:> 7d}'
-        return out
+        return out + f"|{value:> 7d}"
 
 
+@dataclass
 class Convergence:
-    def __init__(self, steps, criteria, program='orca'):
-        """
-        Stores multiple geometry convergence steps
-        :param steps: list of Steps
-        :param criteria: criteria for convergence
-        :param program: the program the results are from
-        """
-        self.steps = steps
-        self.criteria = criteria
-        self.program = program
+    """
+    Stores multiple geometry convergence steps
+    :param steps: list of Steps
+    :param criteria: criteria for convergence
+    :param program: the program the results are from
+    """
+
+    steps: list[Step]
+    criteria: list
+    program: str = "orca"
 
     def __iter__(self):
         yield from self.steps
 
-    def __str__(self):
-        if self.program == 'orca':
-            header = "      Δ energy  RMS grad  MAX grad  RMS step  MAX Step | SCF Steps\n"
-        else:
-            raise NotImplementedError('Convergence currently only implemented for ORCA')
+    def __len__(self):
+        return len(self.steps)
 
-        line = '-'*66 + '\n'
+    def __str__(self):
+        if self.program != "orca":
+            raise NotImplementedError("Convergence currently only implemented for ORCA")
+
+        header = "      Δ energy  RMS grad  MAX grad  RMS step  MAX Step | SCF Steps\n"
+
+        line = "-" * 66 + "\n"
         out = header + line
         for i, step in enumerate(self):
-            out += f'{i:>3}: '
-            for (key, value), criterion in zip(step.params.items(), step.criteria):
-                # integers
-                if key in ['scf_steps']:
+            out += f"{i:>3}: "
+            for (key, value), criterion in step:
+                if key in ["scf_steps"]:
                     pass
-                else:
-                    star = ' '
-                    if abs(value) < criterion and not (i == 0 and key == 'delta_e'):
-                        star = '*'
-                    out += f'{value:> 9.2e}{star}'
-            out += f'|{step.scf_steps:> 7d}\n'
+                star = " "
+                if abs(value) < criterion and (i != 0 or key != "delta_e"):
+                    star = "*"
+                out += f"{value:> 9.2e}{star}"
+            out += f"|{step.scf_steps:> 7d}\n"
 
-        return out + line + '    ' + (' {:> 9.2e}'*len(self.criteria)).format(*self.criteria)
+        form = " {:> 9.2e}" * len(self.criteria)
+        return out + line + "    " + form.format(*self.criteria)
 
-    def plot(self, show=True):
+    def plot(self, show: bool = True):
         """
         Generate a plot of the convergence
         :param show: show the plot
         """
         from matplotlib import pyplot as plt
 
-        f, (ax0, ax1) = plt.subplots(1, 2, sharex='col')
-        f.suptitle('Convergence', fontsize=16)
-        plt.xlabel('Step')
-        x = range(len(self.steps))
+        f, (ax0, ax1) = plt.subplots(1, 2, sharex="col")
+        f.suptitle("Convergence", fontsize=16)
+        plt.xlabel("Step")
+        x = np.arange(len(self))
 
-        ax0.set_yscale('symlog', linthreshy=1e-5)
-        ax0.set_title(r'$\Delta$ Energy')
-        ax0.plot(x, self.delta_e, label='Energy')
+        ax0.set_yscale("symlog", linthreshy=1e-5)
+        ax0.set_title(r"$\Delta$ Energy")
+        ax0.plot(x, self.delta_e, label="Energy")
         ax0.legend()
 
         ax1.set_ylim(0, 1)
-        ax1.set_yscale('symlog', linthreshy=1e-4)
-        ax1.set_title('Convergence Parameters')
-        ax1.plot(x, self.rms_grad, 'b-',  label='RMS Grad')
-        ax1.plot(x, self.max_grad, 'b--', label='Max Grad')
-        ax1.plot(x, self.rms_step, 'r-',  label='RMS Step')
-        ax1.plot(x, self.max_step, 'r--', label='Max Step')
+        ax1.set_yscale("symlog", linthreshy=1e-4)
+        ax1.set_title("Convergence Parameters")
+        ax1.plot(x, self.rms_grad, "b-", label="RMS Grad")
+        ax1.plot(x, self.max_grad, "b--", label="Max Grad")
+        ax1.plot(x, self.rms_step, "r-", label="RMS Step")
+        ax1.plot(x, self.max_step, "r--", label="Max Step")
         # TODO: generalize for more than ORCA
-        ax1.plot(x, [self.criteria[1]]*len(self.steps), 'k-')
-        ax1.plot(x, [self.criteria[1]]*len(self.steps), 'b*')
-        ax1.plot(x, [self.criteria[2]]*len(self.steps), 'k--')
-        ax1.plot(x, [self.criteria[2]]*len(self.steps), 'b*')
-        ax1.plot(x, [self.criteria[3]]*len(self.steps), 'k-')
-        ax1.plot(x, [self.criteria[3]]*len(self.steps), 'r*')
-        ax1.plot(x, [self.criteria[4]]*len(self.steps), 'k--')
-        ax1.plot(x, [self.criteria[4]]*len(self.steps), 'r*')
+        ax1.plot(x, [self.criteria[1]] * len(self), "k-")
+        ax1.plot(x, [self.criteria[1]] * len(self), "b*")
+        ax1.plot(x, [self.criteria[2]] * len(self), "k--")
+        ax1.plot(x, [self.criteria[2]] * len(self), "b*")
+        ax1.plot(x, [self.criteria[3]] * len(self), "k-")
+        ax1.plot(x, [self.criteria[3]] * len(self), "r*")
+        ax1.plot(x, [self.criteria[4]] * len(self), "k--")
+        ax1.plot(x, [self.criteria[4]] * len(self), "r*")
         ax1.legend()
 
         if show:
@@ -105,24 +114,24 @@ class Convergence:
 
     @property
     def delta_e(self):
-        return np.array([step.delta_e for step in self.steps])
+        return np.fromiter((step.delta_e for step in self), dtype=float)
 
     @property
     def rms_e(self):
-        return np.array([step.rms_e for step in self.steps])
+        return np.fromiter((step.rms_e for step in self), dtype=float)
 
     @property
     def max_grad(self):
-        return np.array([step.max_grad for step in self.steps])
+        return np.fromiter((step.max_grad for step in self), dtype=float)
 
     @property
     def rms_grad(self):
-        return np.array([step.rms_grad for step in self.steps])
+        return np.fromiter((step.rms_grad for step in self), dtype=float)
 
     @property
     def max_step(self):
-        return np.array([step.max_step for step in self.steps])
+        return np.fromiter((step.max_step for step in self), dtype=float)
 
     @property
     def rms_step(self):
-        return np.array([step.rms_step for step in self.steps])
+        return np.fromiter((step.rms_step for step in self), dtype=float)
